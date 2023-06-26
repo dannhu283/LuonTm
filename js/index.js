72bf67c5029a79d8ceb71826bc59a3ad6e1edf13
dannhu283 <dannhu2803@gmail.com>
2023-06-26 21:28:00 +0700

    add UX

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function calculator() {
+  let productivity = +document.getElementById("productivity").value;
+  let point = +document.getElementById("point").value;
+  let workDay = +document.getElementById("workDay").value;
+  let nightShift = +document.getElementById("nightShift").value;
+  let salary = 0;
+  if (partTime.checked) {
+  }
+}
